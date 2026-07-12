e3ce866ceac493088b1bca0c8f8c470038319f26
htijna <anjithkj51@gmail.com>
2026-07-12 23:27:11 +0530

    Disable service worker caching and force fresh app loads

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,5 +1,15 @@
-const CACHE = 'civicfix-v1';
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/']))));
-self.addEventListener('fetch', event => {
-  if (event.request.method === 'GET') event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
 });
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,4 +9,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><BrowserRouter><App /><Toaster position="top-right" /></BrowserRouter></React.StrictMode>
 );
 
-if ('serviceWorker' in navigator && import.meta.env.PROD) navigator.serviceWorker.register('/sw.js');
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations()
+    .then(registrations => Promise.all(registrations.map(registration => registration.unregister())))
+    .catch(() => {});
+}
+
+if ('caches' in window) {
+  caches.keys()
+    .then(keys => Promise.all(keys.filter(key => key.startsWith('civicfix')).map(key => caches.delete(key))))
+    .catch(() => {});
+}
